652682f587d7e219761dddd6e7dcf89ac08cd7e3
Karim Abouchalache <kfa4vw@virginia.edu>
2019-11-27 12:16:55 +0100

    A lot of back done

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,30 @@
 class TeamsController < ApplicationController
+  before_action :set_league, only: [:create, :new]
+  def show
+  end
+
   def new
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(team_params)
+    @team.league = @league
+    @team.user = current_user
+    if @team.save
+      redirect_to team_path(@team)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :global_score)
+  end
+
+  def set_league
+    @league = League.find(params[:league_id])
   end
 end
