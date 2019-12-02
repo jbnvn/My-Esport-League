3ba4d196fb1cfd4e8f4a59157c82821501c2695f
Karim Abouchalache <kfa4vw@virginia.edu>
2019-12-02 15:20:13 +0100

    If on modals and button validate

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -7,6 +7,17 @@ class BidsController < ApplicationController
   # end
 
   def create
+  # Validate my bets if all players are chosen
+  # Validate my best. turn all players status into pending
+  # Check if all partcipants players are pending
+  # Compare points by players
+  # If points higher are alone turn player status into validate
+  # Else turn players status into failed
+  # If all players validate then go into 'wait for others to finish mercato'
+  # Else go to round 2
+  # Round 2: remove all players chosen and chose from roles you don't have
+  # go back to first step until all partcipants validate step 7
+  # When all validate step seven go to controller of rounds
   raise
   end
 
