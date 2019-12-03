1341ae704509e7d77e64e5fc87ac883aa2b3bca5
Radomir Zekovik <liquidraso@gmail.com>
2019-12-03 11:49:16 +0100

    Clean Seeds done, may be used for the Demo day

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+League.destroy_all
+Participation.destroy_all
+Team.destroy_all
+Bid.destroy_all
+
+User.create!(email: "JulienB@gmail.com", password: "azerty", username: "JulienB")
+User.create!(email: "CorentinC@gmail.com", password: "azerty", username: "CorentinC")
+User.create!(email: "KarimA@gmail.com", password: "azerty", username: "KarimA")
+User.create!(email: "RadomirZ@gmail.com", password: "azerty", username: "RadomirZ")
+
+League.create!(name: "Le Wagon" , user_id: User.first.id)
+
+Participation.create!(user_id: User.second.id, league_id: League.first.id)
+Participation.create!(user_id: User.third.id, league_id: League.first.id)
+Participation.create!(user_id: User.last.id, league_id: League.first.id)
+
+Team.create!(name: "Team J", league_id: League.first.id, user_id: User.first.id)
+Team.create!(name: "Team C", league_id: League.first.id, user_id: User.second.id)
+Team.create!(name: "Team K", league_id: League.first.id, user_id: User.third.id)
+Team.create!(name: "Team R", league_id: League.first.id, user_id: User.last.id)
+
+Bid.create!(status: "succeeded", points: 10, player_id: Player.where(role: "top").first.id, team_id: Team.first.id)
+Bid.create!(status: "succeeded", points: 11, player_id: Player.where(role: "jun").first.id, team_id: Team.first.id)
+Bid.create!(status: "succeeded", points: 12, player_id: Player.where(role: "mid").first.id, team_id: Team.first.id)
+Bid.create!(status: "succeeded", points: 13, player_id: Player.where(role: "adc").first.id, team_id: Team.first.id)
+Bid.create!(status: "succeeded", points: 14, player_id: Player.where(role: "sup").first.id, team_id: Team.first.id)
+Bid.create!(status: "succeeded", points: 15, player_id: 70, team_id: Team.first.id)
+
+Bid.create!(status: "succeeded", points: 18, player_id: Player.where(role: "top").second.id, team_id: Team.second.id)
+Bid.create!(status: "succeeded", points: 101, player_id: Player.where(role: "jun").second.id, team_id: Team.second.id)
+Bid.create!(status: "succeeded", points: 104, player_id: Player.where(role: "mid").second.id, team_id: Team.second.id)
+Bid.create!(status: "succeeded", points: 150, player_id: Player.where(role: "adc").second.id, team_id: Team.second.id)
+Bid.create!(status: "succeeded", points: 170, player_id: Player.where(role: "sup").second.id, team_id: Team.second.id)
+Bid.create!(status: "succeeded", points: 310, player_id: 71, team_id: Team.second.id)
+
+Bid.create!(status: "succeeded", points: 190, player_id: Player.where(role: "top").third.id, team_id: Team.third.id)
+Bid.create!(status: "succeeded", points: 100, player_id: Player.where(role: "jun").third.id, team_id: Team.third.id)
+Bid.create!(status: "succeeded", points: 110, player_id: Player.where(role: "mid").third.id, team_id: Team.third.id)
+Bid.create!(status: "succeeded", points: 210, player_id: Player.where(role: "adc").third.id, team_id: Team.third.id)
+Bid.create!(status: "succeeded", points: 230, player_id: Player.where(role: "sup").third.id, team_id: Team.third.id)
+Bid.create!(status: "succeeded", points: 109, player_id: 72, team_id: Team.third.id)
+
+Bid.create!(status: "succeeded", points: 50, player_id: Player.where(role: "top").last.id, team_id: Team.last.id)
+Bid.create!(status: "succeeded", points: 78, player_id: Player.where(role: "jun").last.id, team_id: Team.last.id)
+Bid.create!(status: "succeeded", points: 34, player_id: Player.where(role: "mid").last.id, team_id: Team.last.id)
+Bid.create!(status: "succeeded", points: 23, player_id: Player.where(role: "adc").last.id, team_id: Team.last.id)
+Bid.create!(status: "succeeded", points: 38, player_id: Player.where(role: "sup").last.id, team_id: Team.last.id)
+Bid.create!(status: "succeeded", points: 40, player_id: 73, team_id: Team.last.id)
