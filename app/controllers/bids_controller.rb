5d08e63204cc3248aaae839e65cc976bf25e8ecc
Radomir Zekovik <liquidraso@gmail.com>
2019-12-04 19:06:30 +0100

    Jpp

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -33,30 +33,29 @@ class BidsController < ApplicationController
     end
 
     # autres équipes
-    if !(@team.league.teams.count * 6 == @team.league.bids.succeeded.count)
+    #if !(@team.league.teams.count * 6 == @team.league.bids.succeeded.count)
       if @team.league.teams.count * 6 == @team.league.bids.pending.count
         @team.league.players.each do |player|
           player_bids = player.bids.where(team: @team.league.teams).order(points: :desc)
           player_bids.first.succeeded!
           player_bids[1..-1].each{ |pb| pb.failed! }
         end
-
-      elsif @team.league.teams.count * 6 == @team.league.bids.pending.count + @team.league.bids.succeeded.count
-        @team.league.players.each do |player|
-          player_bids = player.bids.where(team: @team.league.teams).order(points: :desc)
-          player_bids.first.succeeded!
-          player_bids[1..-1].each{ |pb| pb.failed! }
-        end
-        if @team.league.teams.count * 6 == @team.league.bids.succeeded.count
           redirect_to show_post_mercato_path(@team)
 
+  #     elsif @team.league.teams.count * 6 == @team.league.bids.pending.count + @team.league.bids.succeeded.count
+  #       @team.league.players.each do |player|
+  #         player_bids = player.bids.where(team: @team.league.teams).order(points: :desc)
+  #         player_bids.first.succeeded!
+  #         player_bids[1..-1].each{ |pb| pb.failed! }
+  #       end
+  #       if @team.league.teams.count * 6 == @team.league.bids.succeeded.count
+  #         redirect_to show_post_mercato_path(@team)
+
         end
-      end
-        redirect_to team_path(@team)
-    else
-      redirect_to show_post_mercato_path(@team)
-    end
-  end
+  #     end
+  #       redirect_to team_path(@team)
+  #   #else
+   end
   # Round 2: remove all players chosen and chose from roles you don't have
     # if @team.league.bids.failed == 0
     # redirect to show_post_mercato
