e40d7ea34de86a9902adabecface8bc0b02f8714
Karim Abouchalache <kfa4vw@virginia.edu>
2019-12-04 15:20:09 +0100

    i forgot what i Did

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get 'show_post_mercato/:id', to: 'teams#show_post_mercato'
+  get 'show_post_mercato/:id', to: 'teams#show_post_mercato', as: "show_post_mercato"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :leagues do
     resources :teams
